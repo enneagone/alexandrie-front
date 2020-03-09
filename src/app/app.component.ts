fe6fb6eda954cd064e59eaa061f6bf3ca9e82c2f
Nicolas Beaussart <nic.beaussart@gmail.com>
2020-03-09 17:45:26 +0100

    :tada: Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'alx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'alexandrie-front';
+}
