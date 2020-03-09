679a23d9b321c045e9e4329792013d561ae764ca
Nicolas Beaussart <nic.beaussart@gmail.com>
2020-03-09 17:46:30 +0100

    :art: apply prettier style to project

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,8 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('alx-root .content span')).getText() as Promise<string>;
+    return element(by.css('alx-root .content span')).getText() as Promise<
+      string
+    >;
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'alx-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'alexandrie-front';
