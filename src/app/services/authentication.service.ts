d583907cd79f8d30e6ea556f0f81c495538c4408
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2020-05-04 19:03:26 +0200

    :bug: fix probleme with back end for request

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rx probleme js';
 import { map } from 'rxjs/operators';
 import { element } from 'protractor';
+import { query } from '@angular/animations';
 
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
   private currentUserSubject: BehaviorSubject<any>;
   public currentUser: Observable<any>;
-  private config = 'http://localhost:8080';
+  /*
+   * maintenant l'adresse est dans le fichier proxy.config.js
+   * pour l'une des probleme, il y avait un probleme entre http et httpS
+   * et le second avec la requete post, a fonctionné en créant ce fichier de config
+   * et il faut demarrer avec ng serve --proxy-config src/proxy.config.json
+   * cf, ou j'ai trouvé la soluce : https://stackoverflow.com/questions/60169694/java-rest-call-get-url-neterr-failed
+   */
+  private config = '';
 
   constructor(private http: HttpClient) {
     // @ts-ignore
@@ -40,19 +48,20 @@ export class AuthenticationService {
   }
 
   register(email: string, username: string, password: string) {
-    const params = new URLSearchParams();
-    params.set('username', username);
-    params.set('email', email);
-    params.set('password', password);
+    const parameters = new HttpParams();
+    parameters.append('username', username);
+    parameters.append('email', email);
+    parameters.append('password', password);
 
     return this.http
-      .post(this.config + `/public/users/register`, { params })
+      .post(`/public/users/register`, {}, { params: parameters })
       .subscribe(
         (result) => {
           console.log(result);
         },
         (error) => {
-          console.log('echec');
+          /* ca marche mais ca passe quand meme dans echec :-/ */
+          console.log('echec du register');
         },
       );
   }
