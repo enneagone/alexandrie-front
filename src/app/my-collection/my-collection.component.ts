582b59a01812ae195648c56ad8167a19c53955a5
zaydane-mokni <zaydane95@gmail.com>
2020-05-25 10:01:31 +0200

    :construction: adding my collection component

---
+++ src/app/my-collection/my-collection.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from '../create-media-modal';
+
+@Component({
+  selector: 'alx-my-collection',
+  templateUrl: './my-collection.component.html',
+  styleUrls: ['./my-collection.component.scss'],
+})
+export class MyCollectionComponent implements OnInit {
+  bodyText: string;
+
+  constructor(private modalService: ModalService) {}
+
+  ngOnInit() {
+    this.bodyText = 'This text can be updated in modal 1';
+  }
+
+  openModal(id: string) {
+    this.modalService.open(id);
+  }
+
+  closeModal(id: string) {
+    this.modalService.close(id);
+  }
+}
