20e1edc2a311572d5b85d6a3e0e6c9781dd012c7
zaydane-mokni <zaydane95@gmail.com>
2020-05-25 10:00:39 +0200

    :construction: adding create media modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,24 @@ import { AuthModule } from './auth/auth.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core';
+import { ModalModule } from './create-media-modal';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeModule } from './home/home.module';
+import { MyCollectionComponent } from './my-collection/my-collection.component';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LayoutModule } from './layout/layout.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MyCollectionComponent],
   imports: [
     BrowserModule,
     AuthModule,
     HomeModule,
     AppRoutingModule,
+    ModalModule,
     ReactiveFormsModule,
     HttpClientModule,
     CoreModule,
