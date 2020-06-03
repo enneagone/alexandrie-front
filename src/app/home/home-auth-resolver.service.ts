bdeb7583fe546b837b39094fcae2961fed07e978
antoine <aberthier15@gmail.com>
2020-06-03 15:12:35 +0200

    :bug: fix validation token

--- src/app/home/home-auth-resolver.service.ts
+++ src/app/home/home-auth-resolver.service.ts
@@ -8,13 +8,19 @@ import {
 } from '@angular/router';
 
 import { JwtService } from '../core';
+import { UserService } from '../core/services';
 
 @Injectable()
 export class HomeAuthResolver implements CanActivate {
-  constructor(private router: Router, private jwtService: JwtService) {}
+  constructor(
+    private router: Router,
+    private jwtService: JwtService,
+    private authenticationService: UserService,
+  ) {}
 
   // @ts-ignore
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    this.authenticationService.userKnown();
     if (this.jwtService.getToken()) {
       return true;
     } else {
