0c3eff99938d801478fc3d1e186f70adc10ffee0
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2020-06-05 14:30:12 +0200

    :construcion: pregrossing
    i am trying to get data from the api, i have a 'mock' file too

--- src/app/core/services/collection.service.ts
+++ src/app/core/services/collection.service.ts
@@ -2,15 +2,31 @@ import { Injectable } from '@angular/core';
 import { Element } from '@angular/compiler';
 import { Observable, of } from 'rxjs';
 import { ELEMENT } from './mock-element';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { catchError } from 'rxjs/operators';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CollectionService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   // TODO bien sur faire une vraie requete, il faudra ^
-  getElements() {
+  /*getElements() {
     return of(ELEMENT);
+  }*/
+
+  // pour le moment observable any mais il faudra peut etre preciser
+  // le path c'est l'url de l'adresse de l'api
+  /* get elements from the collection of medias */
+  getElements(
+    path: string,
+    params: HttpHeaders = new HttpHeaders(),
+  ): Observable<any> {
+    return this.http
+      .get(`${environment.api_url}${path}`, { headers: params })
+      .pipe(catchError(ApiService.formatErrors));
   }
 }
